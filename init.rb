bd0258585585b954a91d08519fed15b4d83ce97e
Matt Sears <matt@littlelines.com>
2008-08-10 20:26:42 -0400

    Added script to install javascript and stylesheet upon installation

--- init.rb
+++ init.rb
@@ -14,9 +14,11 @@ end
 
 # install files
 unless File.exists?(RAILS_ROOT + '/public/javascripts/validating_form.js')
-    dir = '/public/javascripts/'
-    source = File.join(directory, dir)
+  ['/public/javascripts', '/public/stylesheets'].each do |dir|
+    source = File.join(directory,dir)
     dest = RAILS_ROOT + dir
+    FileUtils.mkdir_p(dest)
     FileUtils.cp(Dir.glob(source+'/*.*'), dest)
   end
 end
+
